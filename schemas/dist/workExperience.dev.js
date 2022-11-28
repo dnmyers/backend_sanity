f1863e23ade280baa3c81fbbcb684e67d464a354
Daniel Myers <daniel.neil.myers@gmail.com>
2022-11-28 02:10:23 -0600

    first commit

---
+++ schemas/dist/workExperience.dev.js
@@ -0,0 +1,26 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
+  name: 'workExperience',
+  title: 'Work Experience',
+  type: 'document',
+  fields: [{
+    name: 'name',
+    title: 'name',
+    type: 'string'
+  }, {
+    name: 'company',
+    title: 'Company',
+    type: 'string'
+  }, {
+    name: 'desc',
+    title: 'Desc',
+    type: 'string'
+  }]
+};
+exports["default"] = _default;
+//# sourceMappingURL=workExperience.dev.js.map
